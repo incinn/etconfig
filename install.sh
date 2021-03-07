06c6f138b1b71017ac8a7c1e9957ec888c04bee5
Barnz <hi@barnz.dev>
2021-03-07 13:10:37 +0000

    Copy don't move

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mv ./autoexec.cfg ~/.etwolf/etmain/ || { echo "Failed to move autoexe.cfg" >&2; exit 1; } && echo "Moved autoexe.cfg"
-mv ./cfg/allies.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to move cfg/allies.cfg" >&2; exit 1; } && echo "Moved cfg/allies.cfg"
-mv ./cfg/axis.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to move cfg/axis.cfg" >&2; exit 1; } && echo "Moved cfg/axis.cfg"
+cp -f ./autoexec.cfg ~/.etwolf/etmain/ || { echo "Failed to copy autoexec.cfg" >&2; exit 1; } && echo "Copied autoexec.cfg"
+cp -f ./cfg/allies.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/allies.cfg" >&2; exit 1; } && echo "Copied cfg/allies.cfg"
+cp -f ./cfg/axis.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/axis.cfg" >&2; exit 1; } && echo "Copied cfg/axis.cfg"
 
 echo "Complete"
 exit 1
