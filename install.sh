136858cba883ddc02dfb12d5dc26f05da7b43d77
Barnz <hi@barnz.dev>
2021-03-07 13:33:09 +0000

    FG42 before GRAND in class config

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo "Installing ET configuration files to ~/.etwolf/etmain/"
 
-cp -f ./autoexec.cfg ~/.etwolf/etmain/ || { echo "Failed to copy autoexec.cfg" >&2; exit 1; } && echo "Copied autoexec.cfg"
-cp -f ./cfg/allies.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/allies.cfg" >&2; exit 1; } && echo "Copied cfg/allies.cfg"
-cp -f ./cfg/axis.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/axis.cfg" >&2; exit 1; } && echo "Copied cfg/axis.cfg"
+cp -f ./autoexec.cfg ~/.etwolf/etmain/ || { echo "Failed to copy autoexec.cfg" >&2; exit 1; } && echo "...copied autoexec.cfg"
+cp -f ./cfg/allies.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/allies.cfg" >&2; exit 1; } && echo "...copied cfg/allies.cfg"
+cp -f ./cfg/axis.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/axis.cfg" >&2; exit 1; } && echo "...copied cfg/axis.cfg"
 
 echo "Complete"
 exit 1
