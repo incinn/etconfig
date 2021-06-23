005823e1f97c3fdd737676e15b1750b6901fe3db
Barnz <hi@barnz.dev>
2021-06-24 00:41:22 +0100

    remove install script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Installing ET configuration files to ~/.etwolf/etmain/"
-
-cp -f ./barnz.cfg ~/.etwolf/etmain/ || { echo "Failed to copy barnz.cfg" >&2; exit 1; } && echo "...copied barnz.cfg"
-cp -f ./cfg/allies.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/allies.cfg" >&2; exit 1; } && echo "...copied cfg/allies.cfg"
-cp -f ./cfg/axis.cfg ~/.etwolf/etmain/cfg/ || { echo "Failed to copy cfg/axis.cfg" >&2; exit 1; } && echo "...copied cfg/axis.cfg"
-
-echo "Complete"
-exit 1
